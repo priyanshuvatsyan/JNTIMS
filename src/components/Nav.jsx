0ba859b6e2784c8ba5e4cc46deae587a15ae44c6
Priyanshu <56836660+priyanshuvatsyan@users.noreply.github.com>
2025-09-02 12:55:22 +0530

    Fixed SPA Routing Problem

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -6,7 +6,7 @@ export default function Nav() {
   return (
     <nav className="sidebar">
       <div className="logo">
-        <img src="jnt logo.png" alt="Logo" />
+        <img src="/jnt logo.png" alt="Logo" />
       </div>
       <ul className="nav-links">
         <li><Link to="/">Home</Link></li>
